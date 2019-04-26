b00b6d10560dfd5c9faf6114a36f3427abadab33
Takeshi Kameyama <tkame123@gmail.com>
2019-04-26 15:26:32 +0900

    singleton

--- study/7_singleton/7.1.3.js
+++ study/7_singleton/7.1.3.js
@@ -11,3 +11,25 @@ function Universe() {
 
     // return this;
 }
+
+ // re
+function Universe() {
+
+    var instance;
+
+    Universe = function Universe() {
+        return instance;
+    };
+
+    Universe.prototype = this;
+
+    instance = new Universe();
+
+    instance.constructor = Universe;
+
+    instance.start_time = 0;
+    instance.bang = "Big";
+
+    return instance;
+
+}
