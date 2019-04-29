f3fb55f3b3122f8081f5b9f37351e0d94b58182d
toru kameyama <tkameyamax@gmail.com>
2019-04-29 11:14:51 +0900

    singleton

--- src/singleton/singleton.test.ts
+++ src/singleton/singleton.test.ts
@@ -4,6 +4,8 @@ describe('singletonTest', () => {
     test('equal', () => {
         const uni = Universe.instance;
         const uni2 = Universe.instance;
+        expect(uni).toBeDefined();
+        expect(uni2).toBeDefined();
         expect(uni === uni2).toBe(true);
     })
 });
