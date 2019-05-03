680e9a298ab73f195767430afae3f78cd83019c4
Takeshi Kameyama <tkame123@gmail.com>
2019-05-03 12:31:39 +0900

    firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,23 +1,12 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+
 admin.initializeApp();
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 
 export const sendTestMsgNotification = functions.database.ref('/tokens/pwatest')
     .onWrite(async (change, context) => {
 
-     // 必要な値をDatabaseから取得
-     const token = 'testToken';
-
-
-     // FCMへ通知実施
+        const token: string = change.after.val().token;
 
         // 通知のJson.
         const payload = {
@@ -27,6 +16,7 @@ export const sendTestMsgNotification = functions.database.ref('/tokens/pwatest')
             }
         };
 
+        // FCMへの通知
         // https://firebase.google.com/docs/cloud-messaging/admin/legacy-fcm?hl=ja
         admin.messaging().sendToDevice(token, payload)
             .then((response: any) => {
