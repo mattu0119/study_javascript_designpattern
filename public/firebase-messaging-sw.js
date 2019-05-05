3800f33cef2b330408e61ed17992742b852d217e
Takeshi Kameyama <tkame123@gmail.com>
2019-05-06 07:39:57 +0900

    Android Notification Test

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -14,16 +14,39 @@ firebase.initializeApp({
 // messages.
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function(payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    // Customize notification here
-    var notificationTitle = `back:${payload.data.title}`;
-    var notificationOptions = {
-        body: payload.data.body,
-        // icon: '/firebase-logo.png'
-    };
+// messaging.setBackgroundMessageHandler(function(payload) {
+//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
+//     // Customize notification here
+//     var notificationTitle = `back:${payload.data.title}`;
+//     var notificationOptions = {
+//         body: payload.data.body,
+//         // icon: '/firebase-logo.png'
+//     };
+//
+//     return self.registration.showNotification(
+//         notificationTitle,
+//         notificationOptions);
+// });
 
-    return self.registration.showNotification(
-        notificationTitle,
-        notificationOptions);
+self.addEventListener('push', function(event) {
+    console.log('Received a push message', event);
+
+    var title = JSON.parse(event.data.text()).data.title;
+    var body = JSON.parse(event.data.text()).data.body;
+    // var icon = '/images/icon-192x192.png';
+    // var tag = 'simple-push-demo-notification-tag';
+    // var data = {
+    //     doge: {
+    //         wow: 'such amaze notification data'
+    //     }
+    // };
+
+    event.waitUntil(
+        self.registration.showNotification(title, {
+            body: body,
+            // icon: icon,
+            // tag: tag,
+            // data: data
+        })
+    );
 });
